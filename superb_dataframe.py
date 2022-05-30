1d21818421e616c327136b0d15186872480ed663
Mikhail <fff_khhh@mail.ru>
2022-05-30 13:22:02 +0700

    add comments

--- superb_dataframe.py
+++ superb_dataframe.py
@@ -1,3 +1,9 @@
+from typing import (
+    Union,
+    Callable,
+    Iterable
+)
+
 from numpy import NaN
 from pandas import DataFrame
 from scipy.stats import bootstrap
@@ -14,27 +20,51 @@ from correlation import (
 
 class SuperbDataFrame(DataFrame):
 
-    def corr_and_p_value(self, corr_method=dropna_pearsonr,
+    def corr_and_p_value(self, corr_function: Callable[[Iterable, Iterable], tuple[float, float]] = dropna_pearsonr,
                          r_decimals: int = 2, p_decimals: int = 3) -> DataFrame:
+        """
+        Similar to DataFrame.corr(), but returns correlations between columns with their p-values.
+        Cell format: '0.90\n(p=0.001)'.
+
+        Params:
+            corr_function: Function for correlation calculations (default Pearson).
+                           Signature: corr_function(Iterable, Iterable) -> (float, float)
+            r_decimals: Number of decimal places of the correlation coefficient
+            p_decimals: Number of decimal places of the p-value
+        """
 
         result = DataFrame(columns=self.columns)
         result = result.transpose().join(result, how='outer')
 
         for c1 in self.columns:
             for c2 in self.columns:
-                r, p = corr_method(self[c1], self[c2])
+                r, p = corr_function(self[c1], self[c2])
                 result[c1][c2] = print_r_anp_p(r, p, r_decimals, p_decimals)
 
         return result
 
     def bootstrap_corr(self, bootstrap_parameters: dict,
-                       corr_method=dropna_spearmanr, output_func=print_r_anp_p,
-                       r_decimals: int = 2, p_decimals: int = 3,) -> DataFrame:
+                       corr_function: Callable[[Iterable, Iterable], tuple[float, float]] = dropna_spearmanr,
+                       output_func: Callable[..., Union[str, float]] = print_r_anp_p,
+                       r_decimals: int = 2, p_decimals: int = 3, ) -> DataFrame:
+        """
+        Similar to DataFrame.corr(), but returns bootstrap correlations between columns.
+        Cell format must be described in output_func.
+
+        Params:
+            bootstrap_parameters: Parameters for the scipy.stats.bootstrap()
+            corr_function: Function for correlation calculations (default Pearson).
+                           Signature: corr_function(Iterable, Iterable) -> (float, float)
+            output_func: Function for describing the cell format (default '0.90\n(p=0.001)').
+                         Signature: output_func(r, p, high, low, se, r_decimals, p_decimals) -> str|float
+            r_decimals: Number of decimal places of the correlation coefficient
+            p_decimals: Number of decimal places of the p-value
+        """
         result = DataFrame(columns=self.columns)
         result = result.transpose().join(result, how='outer')
 
         def get_corr(x, y):
-            return corr_method(x, y)[0]
+            return corr_function(x, y)[0]
 
         for c1 in self.columns:
             for c2 in self.columns:
