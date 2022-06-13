372afcc291329d9a271e2e38427249bd1c62fa26
Mikhail <fff_khhh@mail.ru>
2022-06-13 20:24:32 +0700

    fix installation error

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 import setuptools
-import pathlib
-import pkg_resources
-
-with pathlib.Path('requirements.txt').open() as requirements_txt:
-    install_requires = [
-        str(requirement)
-        for requirement
-        in pkg_resources.parse_requirements(requirements_txt)
-    ]
 
 setuptools.setup(
     name='zh_utils',
@@ -16,6 +7,5 @@ setuptools.setup(
     author='Mikhail Zharkov',
     author_email='mzharkov@sfu-kras.ru',
     url='https://github.com/mikewellmeansme/ZhUtils',
-    packages=setuptools.find_packages(),
-    install_requires=install_requires
+    packages=setuptools.find_packages()
 )
