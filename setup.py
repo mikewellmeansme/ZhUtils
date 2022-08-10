a4a08c22373ae420ed29adad97a4f6c2a109951b
Mikhail <fff_khhh@mail.ru>
2022-08-10 15:13:30 +0700

    add install_requires

--- setup.py
+++ setup.py
@@ -7,5 +7,12 @@ setuptools.setup(
     author='Mikhail Zharkov',
     author_email='mzharkov@sfu-kras.ru',
     url='https://github.com/mikewellmeansme/ZhUtils',
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7, <4',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'pandera'
+    ]
 )
