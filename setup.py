6ca9774d3acec34d8ad43c0e8b1f190c201d4916
Mikhail <fff_khhh@mail.ru>
2022-08-16 11:03:18 +0700

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='zhutils',
-    version='0.0.1',
+    version='0.1.0',
     description='A personal collection of tools for data processing',
     author='Mikhail Zharkov',
     author_email='mzharkov@sfu-kras.ru',
