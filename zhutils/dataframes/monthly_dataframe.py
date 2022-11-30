93d84c655876e98bd9ef601c4c369c9c4cb8b682
Mikhail <46995859+mikewellmeansme@users.noreply.github.com>
2022-11-30 20:29:16 +0700

    feat: add creation from wide tables

--- zhutils/dataframes/monthly_dataframe.py
+++ zhutils/dataframes/monthly_dataframe.py
@@ -2,15 +2,68 @@ import pandas as pd
 
 from typing import Optional, List, Union
 from zhutils.common import ComparisonFunction, Months
+from zhutils.dataframes.errors import FileExtentionError
 from zhutils.dataframes.superb_dataframe import SuperbDataFrame
-from zhutils.dataframes.schemas import other_schema, monthly_dataframe_schema
+from zhutils.dataframes.schemas import (
+    other_schema,
+    monthly_long_dataframe_schema,
+    monthly_wide_dataframe_schema
+)
 
 
 class MonthlyDataFrame(SuperbDataFrame):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        monthly_dataframe_schema.validate(self)
+        monthly_long_dataframe_schema.validate(self)
+    
+    @classmethod
+    def from_wide(
+            cls,
+            paths: List[str],
+            clim_indexes: List[str],
+            sheet_names: Optional[List[Union[int, str]]]=None
+        ):
+        """
+        Creates a MonthlyDataFrame from multiple wide tables (csv, xls, xlsx)
+
+        Params:
+            paths: Paths to the wide table files
+            clim_indexes: Names of climate index contained in each file
+            sheet_names: Only for xls / xlsx files. Names of sheets with wide tables
+        """
+
+        sheet_names = [0] * len(paths) or sheet_names
+        if not (len(paths) == len(clim_indexes) == len(sheet_names)):
+            raise ValueError(
+                "Lengths of paths, clim_indexes and sheet_names lists must be the same!"
+            )
+
+        long_dfs = []
+
+        for path, clim_index, sheet_name in zip(paths, clim_indexes, sheet_names): 
+            if path.endswith('.csv'):
+                wide_df = pd.read_csv(path)
+            elif path.endswith('.xls') or path.endswith('.xlsx'):
+                wide_df = pd.read_excel(path, sheet_name)
+            else:
+                raise FileExtentionError(
+                    f"""Wrong file extention for wide monthly dataframe! 
+                    Expected CSV, XLS or XLSX, 
+                    got {path}"""
+                )
+            
+            monthly_wide_dataframe_schema.validate(wide_df)
+
+            long_df = wide_df.melt(id_vars='Year', value_name=clim_index, var_name='Month')
+            long_df['Month'] = long_df['Month'].apply(lambda month: Months[month].value)
+            long_df = long_df.set_index(['Year', 'Month'])
+
+            long_dfs.append(long_df)
+        
+        result = pd.concat(long_dfs, axis=1, join='outer').sort_index().reset_index()
+        
+        return MonthlyDataFrame(result)
     
     def compare_with(
             other: pd.DataFrame,
